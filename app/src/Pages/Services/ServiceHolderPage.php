ee363cf61448534d3d1ea685dc7190d4fc69c970
TheRealAmplifier <coding@jeroenpielage.nl>
2021-01-05 21:57:17 +0100

    removed pricing page (using different process)

--- app/src/Pages/Services/ServiceHolderPage.php
+++ app/src/Pages/Services/ServiceHolderPage.php
@@ -4,11 +4,7 @@ namespace Pages;
 
 use Page;
 use SilverStripe\CMS\Model\SiteTree;
-use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
-use SilverStripe\Forms\TextareaField;
-use SilverStripe\Forms\TextField;
-use SilverStripe\Forms\TreeDropdownField;
 
 class ServiceHolderPage extends Page {
 	private static $table_name = 'ServiceHolderPage';
@@ -16,15 +12,7 @@ class ServiceHolderPage extends Page {
 	private static $icon_class = 'font-icon-box';
 
 	private static $db = [
-		'BannerText'					=> 'HTMLText',
-		'ShowContact'					=> 'Boolean',
-		'ContactTitle'	 			=> 'Varchar',
-		'ContactText'	 				=> 'Text',
-		'ContactLinkText'			=> 'Varchar',
-	];
-
-	private static $has_one = [
-		'ContactLinkTarget'		=> SiteTree::class
+		'BannerText'					=> 'HTMLText'
 	];
 	
 	private static $allowed_children = [
@@ -38,14 +26,6 @@ class ServiceHolderPage extends Page {
 		
 		$fields->addFieldsToTab('Root.Banner', [
 			HTMLEditorField::create('BannerText', 'Banner tekst')
-		]);
-
-		$fields->addFieldsToTab('Root.Contact', [
-			CheckboxField::create('ShowContact', 'Contact sectie tonen op project pagina\'s'),
-			TextField::create('ContactTitle', 'Contact titel'),
-			TextareaField::create('ContactText', 'Contact tekst'),
-			TextField::create('ContactLinkText', 'Contact link tekst'),
-			TreeDropdownField::create('ContactLinkTargetID', 'Contact link doeladres', SiteTree::class),
 		]);
 
 		return $fields;
--- app/src/Pages/Services/ServicePage.php
+++ app/src/Pages/Services/ServicePage.php
@@ -8,7 +8,12 @@ use Pages\ServiceHolderPage;
 use Elements\FeaturedServices;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Assets\File;
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\TreeDropdownField;
 
 class ServicePage extends Page {
 	private static $table_name = 'ServicePage';
@@ -16,11 +21,16 @@ class ServicePage extends Page {
 	private static $icon_class = 'font-icon-check-mark-circle';
 
 	private static $db = [
-		'Summary'						=> 'HTMLText'
+		'Summary'							=> 'HTMLText',
+		'ShowContact'					=> 'Boolean',
+		'ContactTitle'	 			=> 'Varchar',
+		'ContactText'	 				=> 'Text',
+		'ContactLinkText'			=> 'Varchar',
 	];
 
 	private static $has_one = [
-		'Icon'							=> File::class
+		'ContactLinkTarget'		=> SiteTree::class,
+		'Icon'								=> File::class
 	];
 
 	private static $allowed_parent = [
@@ -38,6 +48,14 @@ class ServicePage extends Page {
 			UploadField::create('Icon', 'Icoon')->setFolderName('Service Icons'),
 			HTMLEditorField::create('Summary', 'Samenvatting')->setDescription('Deze tekst wordt gebruikt in de overzichten')->setRows(5)
 		], 'Content');
+
+		$fields->addFieldsToTab('Root.Contact', [
+			CheckboxField::create('ShowContact', 'Contact sectie tonen op project pagina\'s'),
+			TextField::create('ContactTitle', 'Contact titel'),
+			TextareaField::create('ContactText', 'Contact tekst'),
+			TextField::create('ContactLinkText', 'Contact link tekst'),
+			TreeDropdownField::create('ContactLinkTargetID', 'Contact link doeladres', SiteTree::class),
+		]);
 
 		return $fields;
 	}
