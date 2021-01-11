eb1f14bcd9ba9591a25d2eb9a23639ce6dcea2e9
TheRealAmplifier <coding@jeroenpielage.nl>
2021-01-11 21:25:04 +0100

    improvements to styles

--- themes/longneck/javascript/script.js
+++ themes/longneck/javascript/script.js
@@ -1,18 +1,18 @@
 import $ from 'jquery';
-import Headroom from "headroom.js";
 
-// const initHeadroom = function() {
-// 	const headerElement = document.querySelector("body");
-// 	const headroom  = new Headroom(headerElement);
-// 	headroom.init();
-// }
 
-$(document).ready(function(){
-	// initHeadroom();
-	
-	$('.header__toggle').click(function() {
-		$(this).toggleClass('active');
-		$('body').toggleClass('navigation--open');
+	$(document).ready(function() {	
+		$('.header__toggle').click(function() {
+			$(this).toggleClass('active');
+			$('body').toggleClass('navigation--open');
 		});
-});
+	});
 
+	$(window).scroll(function() {
+		let scroll = $(window).scrollTop();
+
+		console.log(scroll);
+		if (scroll >= 100) {
+			$('.header').toggleClass('header--scrolled');
+		} 
+	});
